0906188d485a733c27a2a96e291e932ee1eb2697
KMarshSr <marshallrkhiry@gmail.com>
2023-09-01 14:52:07 -0400

    Convert IP to binary

--- js/calculator.js
+++ js/calculator.js
@@ -6,6 +6,8 @@ function runProgram() {
     if(validateIPAddress()) {
         console.log(convertSubnetMaskToCIDR());
         console.log(convertSubnetMaskToInverseMask());
+        console.log(convertSubnetMaskToBinary());
+        console.log(convertIPAddressToBinary());
     }
 }
 
@@ -110,7 +112,7 @@ function convertSubnetMaskToInverseMask() {
             inverseMask = '0.255.255.255';
             break;
         case '255.128.0.0/9':
-            inverseMask = '0.127.255.255'
+            inverseMask = '0.127.255.255';
             break;
         case '255.192.0.0/10':
             inverseMask = '0.63.255.255';
@@ -185,7 +187,165 @@ function convertSubnetMaskToInverseMask() {
     return inverseMask;
 }
 
-function ()
+function convertSubnetMaskToBinary() {
+    let subnetMaskBinary = '';
+
+    switch(subnetMask.value) {
+        case '255.0.0.0/8':
+            subnetMaskBinary = '11111111.00000000.00000000.00000000';
+            break;
+        case '255.128.0.0/9':
+            subnetMaskBinary = '11111111.10000000.00000000.00000000';
+            break;
+        case '255.192.0.0/10':
+            subnetMaskBinary = '11111111.11000000.00000000.00000000';
+            break;
+        case '255.224.0.0/11':
+            subnetMaskBinary = '11111111.11100000.00000000.00000000';
+            break;
+        case '255.240.0.0/12':
+            subnetMaskBinary = '11111111.11110000.00000000.00000000';
+            break;
+        case '255.248.0.0/13':
+            subnetMaskBinary = '11111111.11111000.00000000.00000000';
+            break;
+        case '255.252.0.0/14':
+            subnetMaskBinary = '11111111.11111100.00000000.00000000';
+            break;
+        case '255.254.0.0/15':
+            subnetMaskBinary = '11111111.11111110.00000000.00000000';
+            break;
+        case '255.255.0.0/16':
+            subnetMaskBinary = '11111111.11111111.00000000.00000000';
+            break;
+        case '255.255.128.0/17':
+            subnetMaskBinary = '11111111.11111111.10000000.00000000';
+            break;
+        case '255.255.192.0/18':
+            subnetMaskBinary = '11111111.11111111.11000000.00000000';
+            break;
+        case '255.255.224.0/19':
+            subnetMaskBinary = '11111111.11111111.11100000.00000000';
+            break;
+        case '255.255.240.0/20':
+            subnetMaskBinary = '11111111.11111111.11110000.00000000';
+            break;
+        case '255.255.248.0/21':
+            subnetMaskBinary = '11111111.11111111.11111000.00000000';
+            break;
+        case '255.255.252.0/22':
+            subnetMaskBinary = '11111111.11111111.11111100.00000000';
+            break;
+        case '255.255.254.0/23':
+            subnetMaskBinary = '11111111.11111111.11111110.00000000';
+            break;
+        case '255.255.255.0/24':
+            subnetMaskBinary = '11111111.11111111.11111111.00000000';
+            break;
+        case '255.255.255.128/25':
+            subnetMaskBinary = '11111111.11111111.11111111.10000000';
+            break;
+        case '255.255.255.192/26':
+            subnetMaskBinary = '11111111.11111111.11111111.11000000';
+            break;
+        case '255.255.255.224/27':
+            subnetMaskBinary = '11111111.11111111.11111111.11100000';
+            break;
+        case '255.255.255.240/28':
+            subnetMaskBinary = '11111111.11111111.11111111.11110000';
+            break;
+        case '255.255.255.248/29':
+            subnetMaskBinary = '11111111.11111111.11111111.11111000';
+            break;
+        case '255.255.255.252/30':
+            subnetMaskBinary = '11111111.11111111.11111111.11111100';
+            break;
+        case '255.255.255.254/31':
+            subnetMaskBinary = '11111111.11111111.11111111.11111110';
+            break;
+        case '255.255.255.255/32':
+            subnetMaskBinary = '11111111.11111111.11111111.11111111';
+            break;
+    }
+    return subnetMaskBinary;
+}
+
+function convertIPAddressToBinary() {
+    let decimals = ipAddress.value.split('.');
+    let octet1 = decimals[0];
+    let octet2 = decimals[1];
+    let octet3 = decimals[2];
+    let octet4 = decimals[3];
+    let quotient = 0;
+    const binaryArray1 = [];
+    const binaryArray2 = [];
+    const binaryArray3 = [];
+    const binaryArray4 = [];
+
+    do {
+        quotient = ~~(octet1 / 2);
+        remainder = octet1 % 2;
+        octet1 = quotient;
+        binaryArray1.push(remainder);
+    }
+    while (quotient !== 0) 
+
+    binaryArray1.reverse();
+
+    while (binaryArray1.length < 8) {
+        binaryArray1.unshift(0);
+    }
+
+    do {
+        quotient = ~~(octet2 / 2);
+        remainder = octet2 % 2;
+        octet2 = quotient;
+        binaryArray2.push(remainder);
+    }
+    while (quotient !== 0) 
+
+    binaryArray2.reverse();
+
+    while (binaryArray2.length < 8) {
+        binaryArray2.unshift(0);
+    }
+
+    do {
+        quotient = ~~(octet3 / 2);
+        remainder = octet3 % 2;
+        octet3 = quotient;
+        binaryArray3.push(remainder);
+    }
+    while (quotient !== 0) 
+
+    binaryArray3.reverse();
+
+    while (binaryArray3.length < 8) {
+        binaryArray3.unshift(0);
+    }
+
+    do {
+        quotient = ~~(octet4 / 2);
+        remainder = octet4 % 2;
+        octet4 = quotient;
+        binaryArray4.push(remainder);
+    }
+    while (quotient !== 0) 
+
+    binaryArray4.reverse();
+
+    while (binaryArray4.length < 8) {
+        binaryArray4.unshift(0);
+    }
+
+    const binaryArray1String = binaryArray1.join("");
+    const binaryArray2String = binaryArray2.join("");
+    const binaryArray3String = binaryArray3.join("");
+    const binaryArray4String = binaryArray4.join("");
+    const finalArrayString = binaryArray1String +  "." + binaryArray2String + "." + binaryArray3String + "." + binaryArray4String;
+    return finalArrayString;
+
+}
 
 calculate.addEventListener('click', runProgram);
 
